9160ed1b682ba0ec260728d7d6ac31bf75ecf7e0
Antonina Kobzar <antonina.kobzar@gmail.com>
2024-03-29 10:44:27 +0200

    mongo

--- mongo.js
+++ mongo.js
@@ -1,19 +1,22 @@
 
 const mongoose = require('mongoose')
 
-// if (process.argv.length<3) {
-//   console.log('give password as argument')
-//   process.exit(1)
-// }
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
 
-// const password = process.argv[2]
+const password = process.argv[2]
+const url =
+  `mongodb+srv://antoninakobzar:${password}@cluster1.wqrslo1.mongodb.net/persons
+  .persons?retryWrites=true&w=majority`
 
-const url ="mongodb+srv://antoninakobzar:QaR6mnOaC3p3rNzo@cluster1.wqrslo1.mongodb.net/"
 
 
 mongoose.set('strictQuery',false)
 
 mongoose.connect(url)
+
 
 const personSchema = new mongoose.Schema({
   name: String,
@@ -23,11 +26,20 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model('Person', personSchema)
 
 const person = new Person({
-  name: 'Adam Dull',
-  number: '1231212',
+  name:'',
+  number:'',
 })
 
 person.save().then(result => {
   console.log('person saved!')
   mongoose.connection.close()
 })
+
+// person.find({}).then(result => {
+//   result.forEach(person => {
+//     console.log(person)
+//   })
+//   mongoose.connection.close()
+// })
+// node mongo.js QaR6mnOaC3p3rNzo Anna 040-1234556
+//node mongo.js QaR6mnOaC3p3rNzo Osa 050-1334559
